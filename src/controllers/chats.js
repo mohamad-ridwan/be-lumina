719e50e47ddfac7b6e0d1ba59ef5a51da00f7a64
mohamad-ridwan <mr643062@gmail.com>
2025-03-30 14:37:45 +0700

    clean code

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -1,6 +1,4 @@
-const mongoose = require('mongoose')
 const chats = require('../models/chats')
-const chatRoom = require('../models/chatRoom')
 const { HTTP_STATUS_CODE } = require('../constant');
 
 exports.getChats = async (req, res, next) => {
