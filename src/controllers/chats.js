1be4df7479e70385a7614d98d51252f15d80a63b
mohamad-ridwan <mr643062@gmail.com>
2025-04-06 18:46:40 +0700

    fix: get streams on chats

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -1,23 +1,70 @@
 const chats = require('../models/chats')
-const { HTTP_STATUS_CODE } = require('../constant');
 
-exports.getChats = async (req, res, next) => {
-    const { userId } = req.query
+exports.getChats = async(req, res)=>{
+    res.set({
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+      });
+      
+      const { userId } = req.query;
 
-    if (!userId || !userId.trim()) {
-        res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({
-            message: 'userId required'
-        })
-        return
-    }
-
-    const chatsCurrently = await chats.find({
-        userIds: { $in: [userId] },
-        latestMessage: { $exists: true }
-    }).sort({ latestMessageTimestamp: -1 })
-
-    res.status(HTTP_STATUS_CODE.OK).json({
-        message: 'Chats Data',
-        data: chatsCurrently
-    })
+      let buffer = [];
+    
+      try {
+        const cursor = chats
+          .find(
+            { 
+                userIds: { $in: [userId] },
+                latestMessage: { $exists: true }
+            }
+        )
+          .sort({latestMessageTimestamp: -1})
+          .batchSize(40)
+          .cursor();
+    
+          for await (const doc of cursor) {
+            buffer.push(doc);
+        
+            if (buffer.length >= 50) {
+              res.write(`data: ${JSON.stringify(buffer)}\n\n`);
+              buffer = [];
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+            }
+          }
+        
+          // Kirim sisa data kalau ada (kurang dari 50)
+          if (buffer.length > 0) {
+            res.write(`data: ${JSON.stringify(buffer)}\n\n`);
+          }
+        
+          // Kirim event selesai
+          res.write(`event: done\ndata: {}\n\n`);
+          res.end();
+      } catch (error) {
+        console.error('Error streaming:', error);
+        res.write(`event: error\ndata: ${JSON.stringify({ message: 'Internal Error' })}\n\n`);
+        res.end();
+      }
 }
+
+// exports.getChats = async (req, res, next) => {
+//     const { userId } = req.query
+
+//     if (!userId || !userId.trim()) {
+//         res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({
+//             message: 'userId required'
+//         })
+//         return
+//     }
+
+//     const chatsCurrently = await chats.find({
+//         userIds: { $in: [userId] },
+//         latestMessage: { $exists: true }
+//     }).sort({ latestMessageTimestamp: -1 })
+
+//     res.status(HTTP_STATUS_CODE.OK).json({
+//         message: 'Chats Data',
+//         data: chatsCurrently
+//     })
+// }
